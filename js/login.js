3651ae248bd6398b512d75612c133b6f72575118
Llewellyn_llh <llewellyn_llh@163.com>
2017-12-21 21:31:57 +0800

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,1 @@
+$('#btn1').click(function() {var username = document.getElementById('username').value;var password = document.getElementById('password').value;if(username != 'llh') {alert("用户名不正确");} else if(password != 'llonghu') {alert("密码不正确");} else {location.href="personalManager.html"}})
